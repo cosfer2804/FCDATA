15081148121911c095334e8f774fe464e60fe182
cosfer2804 <thicosfer@gmail.com>
2022-02-15 23:04:08 +0100

    asdad

--- sql/sql analysis.sql
+++ sql/sql analysis.sql
@@ -57,6 +57,7 @@ ORDER BY average_balance DESC
 LIMIT 10;
 
 -- 9. What is the average balance of all the customers in your data?
+## the null values were filled with 0, so for the SQL query we are going to ignore them. On pynthon we deal with them.
 SELECT avg(average_balance) FROM credit_card_data;
 
 -- 10. In this exercise we will use simple group by to check the properties of some of the categorical variables in our data. Note wherever average_balance is asked, please take the average of the column average_balance:
@@ -78,19 +79,44 @@ GROUP BY Bank_Accounts_Open;
 
 -- 11. Your managers are only interested in the customers with the following properties:
 -- Credit rating medium or high
+SELECT customer_number FROM credit_card_data WHERE credit_rating ='High' OR credit_rating ='Medium';
+
 -- Credit cards held 2 or less
+SELECT customer_number FROM credit_card_data WHERE Credit_Cards_Held<=2;
+
 -- Owns their own home
+SELECT customer_number FROM credit_card_data WHERE Own_Your_Home='Yes';
+
 -- Household size 3 or more
+SELECT customer_number FROM credit_card_data WHERE household_size>=3;
+
 -- For the rest of the things, they are not too concerned. Write a simple query to find what are the options available for them? Can you filter the customers who accepted the offers here?
+SELECT * FROM credit_card_data WHERE offer_accepted='Yes';
 
 -- 12. Your managers want to find out the list of customers whose average balance is less than the average balance of all the customers in the database. Write a query to show them the list of such customers. You might need to use a subquery for this problem.
+SELECT customer_number FROM credit_card_data 
+WHERE Average_Balance<(SELECT avg(Average_Balance) FROM credit_card_data);
 
 -- 13. Since this is something that the senior management is regularly interested in, create a view of the same query.
+create or replace view customer_less_than_full_average as
+SELECT customer_number FROM credit_card_data 
+WHERE Average_Balance<(SELECT avg(Average_Balance) FROM credit_card_data);
 
 -- 14. What is the number of people who accepted the offer vs number of people who did not?
+SELECT ((SELECT count(offer_accepted) FROM credit_card_data WHERE offer_accepted='Yes')/count(offer_accepted))*100
+AS percentage_total_of_yes FROM credit_card_data;
 
 -- 15. Your managers are more interested in customers with a credit rating of high or medium. What is the difference in average balances of the customers with high credit card rating and low credit card rating?
+SELECT (SELECT avg(average_balance) FROM credit_card_data WHERE Credit_Rating = 'High') -
+(SELECT avg(average_balance) FROM credit_card_data WHERE Credit_Rating = 'Low') AS diff_high_low;
 
 -- 16. In the database, which all types of communication (mailer_type) were used and with how many customers?
+SELECT mailer_type, count(customer_number) as number_of_customers FROM credit_card_data GROUP BY mailer_type;
 
 -- 17. Provide the details of the customer that is the 11th least Q1_balance in your database.
+SELECT * FROM (SELECT *, DENSE_RANK() OVER(ORDER BY q1_balance ASC) ranked_q1 FROM credit_card_data)  q1_rank
+WHERE q1_rank.ranked_q1 = 11;
+
+
+
+
